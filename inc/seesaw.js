185818adf0df80b1049a09ca5fe2bb917fa4111e
Carlos Ferreira <c@rlos.rocks>
2016-11-27 18:14:29 +0000

    A few fixes to the test page

--- inc/seesaw.js
+++ inc/seesaw.js
@@ -6,19 +6,20 @@ $(document).ready(function ()
     });
 
     var seesaw = SEESAW.getRoot();
+    seesaw.setBackground('lightblue');
 
-    var comp1 = new SEESAW.Component('');
+    var comp1 = new SEESAW.Component();
 
-    comp1.setBounds(20, 100, 100, 250);
-    comp1.setBackground('blue');
+    comp1.setBounds(20, 100, 300, 250);
+    comp1.setBackground('lightgreen');
 
     seesaw.addChild(comp1);
 
-    var comp2 = new SEESAW.Component('comp-1');
-    seesaw.addChild(comp2);
+    var comp2 = new SEESAW.Component();
+    comp1.addChild(comp2);
 
-    comp2.setBackground('green');
+    comp2.setBackground('salmon');
     comp2.setSize(100, 100);
-    comp2.setPosition(-10, -10);
+    comp2.setPosition(20, 20);
 
 });
