5df27fa10a1adaa5c38ccc9c127af319e981da7b
Carlos Ferreira <c@rlos.rocks>
2016-11-27 18:30:59 +0000

    Few changes to the example

--- inc/seesaw.js
+++ inc/seesaw.js
@@ -2,7 +2,7 @@ $(document).ready(function ()
 {
     SEESAW.setUserConfig({
         fontSize:   '14px',
-        background: 'salmon'
+        background: 'lightblue'
     });
 
     var seesaw = SEESAW.getRoot();
@@ -10,15 +10,15 @@ $(document).ready(function ()
     var comp1 = new SEESAW.Component();
 
     comp1.setBounds(20, 100, 100, 250);
-    comp1.setBackground('blue');
+    comp1.setBackground('lime');
 
     seesaw.addChild(comp1);
 
     var comp2 = new SEESAW.Component();
     seesaw.addChild(comp2);
 
-    comp2.setBackground('green');
+    comp2.setBackground('orange');
     comp2.setSize(100, 100);
-    comp2.setPosition(-10, -10);
+    comp2.setPosition(250, 400);
 
 });
