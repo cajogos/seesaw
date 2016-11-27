b3aaf84b9b7694bc78b0d5f171275c9558705f47
Carlos Ferreira <c@rlos.rocks>
2016-11-27 13:10:10 +0000

    Some changes

--- seesaw/Seesaw.js
+++ seesaw/Seesaw.js
@@ -5,4 +5,7 @@ SEESAW.createdAd = Date.now();
 // True for enable debugging features
 SEESAW.DEVELOPMENT = true;
 
-SEESAW.DEFAULT_CONFIG = {};
+SEESAW.DEFAULT_CONFIG = {
+    fontSize:   '12px',
+    background: '#CCC'
+};
---
+++ seesaw/ui/RootComponent.js
@@ -0,0 +1,13 @@
+/*
+* @class SEESAW.RootComponent
+* @constructor
+* @config {Object} [config]
+* @extends SEESAW.Component
+*/
+SEESAW.RootComponent = function (config)
+{
+
+};
+
+SEESAW.RootComponent.prototype = Object.create(SEESAW.Component.prototype);
+SEESAW.RootComponent.prototype.constructor = SEESAW.RootComponent;
